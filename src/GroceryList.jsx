92ed288160e74693d9efd77e51373b6e00db23e0
Mehdi <chihi.med@gmail.com>
2024-05-05 16:36:25 +0200

    Implementing "adding a product" to the list

--- src/GroceryList.jsx
+++ src/GroceryList.jsx
@@ -1,10 +1,20 @@
-import { AppBar, Box, IconButton, Modal, Toolbar, Typography } from '@mui/material';
+import { AppBar, Autocomplete, Box, Button, IconButton, Modal, Slider, Stack, TextField, Toolbar, Typography } from '@mui/material';
 import { Add } from '@mui/icons-material';
 import { DataGrid } from '@mui/x-data-grid';
 import { Delete } from '@mui/icons-material';
 import React, { useState } from 'react';
 
 const GroceryList = () => {
+
+  const [openAddProduct, setOpenAddProduct] = useState(false);
+  const [quantity, setQuantity] = useState(0);
+  const [importance, setImportance] = useState(0);
+  const [selectedProduct, setSelectedProduct] = useState(products[0]);
+  const [selectedStore, setSelectedStore] = useState(stores[0]);
+
+  const [rows, setRows] = useState([
+    { id: 1, productName: 'Apple', quantity: 10, Store: 'Aldi', importance: 9 },
+  ]);
 
   const columns = [
     { field: 'productName', headerName: 'Product name', flex: 1 },
@@ -23,29 +33,34 @@ const GroceryList = () => {
     }
   ];
 
-  const [rows, setRows] = useState([
-    { id: 1, productName: 'Apple', quantity: 10, Store: 'Aldi', importance: 9 },
-    { id: 2, productName: 'Apple', quantity: 10, Store: 'Aldi', importance: 9 },
-    { id: 3, productName: 'Apple', quantity: 10, Store: 'Aldi', importance: 9 },
-    { id: 4, productName: 'Apple', quantity: 10, Store: 'Aldi', importance: 9 },
-    { id: 5, productName: 'Apple', quantity: 10, Store: 'Aldi', importance: 9 },
-    { id: 6, productName: 'Apple', quantity: 10, Store: 'Aldi', importance: 9 },
-    { id: 7, productName: 'Apple', quantity: 10, Store: 'Aldi', importance: 9 },
-    { id: 8, productName: 'Apple', quantity: 10, Store: 'Aldi', importance: 9 },
-    { id: 9, productName: 'Apple', quantity: 10, Store: 'Aldi', importance: 9 },
-  ]);
+  const handleOpenAddProduct = () => setOpenAddProduct(true);
+  const handleCloseAddProduct = () => setOpenAddProduct(false);
 
   const handleDeleteProduct = (id) => {
     setRows(r => r.filter(row => row.id !== id));
   };
 
-  const handleAddProduct = (product) => {
-    setRows(r => [...r, product]);
+  const handleAddProduct = () => {
+
+    const newProduct = {
+      id: rows.length + 1,
+      productName: selectedProduct,
+      quantity: quantity,
+      Store: selectedStore,
+      importance: importance
+    };
+
+    setRows([...rows, newProduct]);
+
+    setQuantity(0);
+    setImportance(0);
+    setSelectedProduct(products[0]);
+    setSelectedStore(stores[0]);
+    handleCloseAddProduct();
+
   };
 
-  const [open, setOpen] = useState(false);
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
+
 
   return (
     <>
@@ -55,7 +70,7 @@ const GroceryList = () => {
             <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
               Grocery List
             </Typography>
-            <IconButton color="inherit" onClick={handleOpen}>
+            <IconButton color="inherit" onClick={handleOpenAddProduct}>
               <Add />
             </IconButton>
           </Toolbar>
@@ -69,17 +84,79 @@ const GroceryList = () => {
         </div>
       </Box>
       <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
+        open={openAddProduct}
+        onClose={handleCloseAddProduct}
+        sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}
       >
-        <Box>
- ihciduhciuhifdhifh         
-        </Box>
+        <Stack
+          direction="column" justifyContent="center" alignItems="center" width={500} height={400} bgcolor="white" p={3} borderRadius={5} gap={1}>
+          <Typography textAlign="center" color="gray">Add a product to your list</Typography>
+          <Autocomplete
+            id="free-solo-demo"
+            value={selectedProduct}
+            onChange={(event, newValue) => {
+              setSelectedProduct(newValue);
+            }}
+            freeSolo
+            options={products}
+            renderInput={(params) => <TextField {...params} label="Products" />}
+            sx={{ width: "100%" }}
+          />
+
+          <Typography textAlign="center" color="gray" pt={1}>Quantity :</Typography>
+          <Slider defaultValue={1} value={quantity} onChange={e => setQuantity(e.target.value)} max={20} valueLabelDisplay="auto" sx={{ width: "80%" }} />
+          <Autocomplete
+            id="free-solo-demo"
+            value={selectedStore}
+            onChange={(event, newValue) => {
+              setSelectedStore(newValue);
+            }}
+            options={stores}
+            renderInput={(params) => <TextField {...params} label="Stores" />}
+            sx={{ width: "100%" }}
+          />
+          <Typography textAlign="center" color="gray" pt={1}>Importance :</Typography>
+
+          <Slider color="error" defaultValue={1} value={importance} onChange={e => setImportance(e.target.value)} max={10} valueLabelDisplay="auto" sx={{ width: "80%" }} />
+
+          <Box display="flex" justifyContent="space-between" width="100%" mt={4}>
+            <Button variant="contained" color="success" onClick={handleAddProduct}>
+              Add product
+            </Button>
+            <Button variant="contained" color="error" onClick={handleCloseAddProduct}>
+              Cancel
+            </Button>
+          </Box>
+        </Stack>
       </Modal>
     </>
   );
 }
 
+
+const stores = [
+  'Aldi',
+  'Colruyt',
+  'Lidl',
+  'Carrefour',
+  'Cora',
+  'Picard',
+  'Action',
+  'Kruidvat',
+  'Delhaize',
+  'Primark',
+];
+
+const products = [
+  'Apple',
+  'Banana',
+  'Peer',
+  'Cherry',
+  'Chair',
+  'Table',
+  'Paper',
+  'Glass',
+  'Water',
+  'Juice',
+];
 export default GroceryList;
